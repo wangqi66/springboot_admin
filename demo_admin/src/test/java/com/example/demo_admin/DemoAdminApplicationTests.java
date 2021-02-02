4fb01051761aea765b500f35142e7c9a1aeb3d2c
wangqi555 <949526272@qq.com>
2021-02-02 10:17:37 +0800

    jdbctemplete数据库交互demo

--- demo_admin/src/test/java/com/example/demo_admin/DemoAdminApplicationTests.java
+++ demo_admin/src/test/java/com/example/demo_admin/DemoAdminApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.demo_admin;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class DemoAdminApplicationTests {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
     @Test
     void contextLoads() {
+//        List<Map<String, Object>> maps = jdbcTemplate.queryForList("select * from account",);
+        Long o = jdbcTemplate.queryForObject("select count(*) from account", long.class);
+        System.out.println(o);
+
     }
 
 }
